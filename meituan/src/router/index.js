27d69488324bf3d6be464fda49bcff67a41682e8
liunian-123 <1098415909@qq.com>
2020-10-25 11:05:12 +0800

    2020 10.25

--- meituan/src/router/index.js
+++ meituan/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Index from '@/views/home/index'
-import Dindex from '@/views/detail/index'
+import detail from '@/views/detail/index'
 
 Vue.use(VueRouter)
 
@@ -12,7 +12,7 @@ const routes = [
   },
   {
     path:'/detail',
-    component:Dindex
+    component:Detail
   }
 ]
 
