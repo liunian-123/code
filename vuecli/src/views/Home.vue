035863ed0747b9ee21d2681e7f238bf45f420fa8
liunian-123 <1098415909@qq.com>
2020-10-25 09:41:44 +0800

    2020 10.25

--- vuecli/src/views/Home.vue
+++ vuecli/src/views/Home.vue
@@ -3,7 +3,26 @@
         我是首页
         <router-link to="/home/a">a菜单</router-link>
         <router-link to="/home/b">b菜单</router-link>
+        {{
+            $store.state.count
+        }}
 
+        <ul>
+<!-- $store.state.xx     a模块中$store.state.a.xx -->
+            <li v-for="(obj,index) in $store.state.a.list" :key="index">
+                {{obj.name}}
+            </li>   
+        </ul>
+        <ul>
+<!-- obj数组中的每个对象  index 是对象的索引 -->
+            <li v-for="(obj,index) in $store.state.b.prolist" :key="index">
+                {{obj.name}}
+            </li>
+        </ul>
+        <button @click="$store.commit('a/add')">添加赵六</button>
+        <button @click="$store.commit('b/reverse')">颠倒商品列表数组</button>
+        
+        <!-- {{$store.state.b.prolist}} -->
         <router-view></router-view>
     </div>
 </template>
