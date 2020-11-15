fce6fcafed98bad0520d2a8a064dd35442847841
liunian-123 <1098415909@qq.com>
2020-11-15 14:13:52 +0800

    2020 11.15

--- meituan/src/views/order/index.vue
+++ meituan/src/views/order/index.vue
@@ -6,13 +6,11 @@
       <ul>
         <!-- 动态绑定class {class名：true|false} -->
         <li
-          :class="{ active: index == currentIndex, 'cate-list': true }"
-          v-for="(obj, index) in nav"
+          :class="{active:index == currentIndex,'cate-list':true}"
+          v-for="(obj,index) in nav"
           @click="change(index)"
           :key="obj.id"
-        >
-          {{ obj.name }}
-        </li>
+        >{{obj.name}}</li>
       </ul>
     </div>
 
@@ -20,27 +18,28 @@
     <!--  -->
     <div class="pro-box">
       <div>
-        <div class="prod-cate-box" v-for="(obj, index) in goods" :key="index">
-          <h2>{{ obj.name }}</h2>
+        <div class="prod-cate-box" v-for="(obj,typeIndex) in goods" :key="typeIndex">
+          <h2>{{obj.name}}</h2>
           <ul>
-            <li class="prod-list" v-for="prod in obj.content" :key="prod.id">
+            <li class="prod-list" v-for="(prod,index) in obj.content" :key="prod.id">
               <img class="prod-img" :src="prod.img" alt />
               <div>
-                <p>{{ prod.name }}</p>
-                <p>{{ prod.price }}</p>
+                <p>{{prod.name}}</p>
+                <p>{{prod.price}}</p>
               </div>
-              <!-- 加减商品 -->
               <div class="add-cart">
-                <!-- iconfont 文字图标的应用  assert用于存储静态文件 -->
-                <span class="iconfont icon-jianhao"></span>
-                <span class="num">0</span>
-                <span class="iconfont icon-jiahao-copy"></span>
+                <span class="iconfont icon-jianhao" v-if="prod.count > 0"></span>
+                <span class="num">{{prod.count}}</span>
+                <!-- 添加 -->
+                <span class="iconfont icon-jiahao-copy" @click="$store.commit('add',{typeIndex,index})"></span>
               </div>
             </li>
           </ul>
         </div>
       </div>
     </div>
+
+
   </div>
 </template>
 
@@ -54,7 +53,7 @@ export default {
       goods: [],
       currentIndex: 0, //记录当前激活的索引
       scrollY: 0, //prodScroll 滚动的高度
-      pos: [], //记录所有分类div的位置prod-cate-box
+      pos: [] //记录所有分类div的位置prod-cate-box
     };
   },
   methods: {
@@ -78,31 +77,33 @@ export default {
         }
       }
       console.log(this.pos);
-    },
+    }
   },
   created() {
     axios
       .get(
         `http://admin.gxxmglzx.com/tender/test/get_nav?id=${this.$route.query.id}`
       )
-      .then((res) => {
+      .then(res => {
         console.log(res.data.data);
         this.nav = res.data.data.nav;
         this.goods = res.data.data.goods;
+        // vuex中存储商品  调用vuex中mutation中的方法
+        this.$store.commit('save',this.goods);
 
         // 数据请求回来
         // 渲染到页面后 -> new BetterScroll
         this.$nextTick(() => {
           this.cateScroll = new BetterScroll(".cate-box", {
             click: true,
-            bounce: false,
+            bounce: false
           });
           this.prodScroll = new BetterScroll(".pro-box", {
             click: true,
             bounce: false,
-            probeType: 3,
+            probeType: 3
           });
-          this.prodScroll.on("scroll", (position) => {
+          this.prodScroll.on("scroll", position => {
             // console.log(position.x, position.y);
             this.scrollY = Math.abs(position.y);
             // console.log(this.scrollY)
@@ -116,21 +117,21 @@ export default {
   watch: {
     scrollY(val) {
       // console.log(val)
-      // val在哪个坐标范围内
-      let cateList = document.querySelectorAll(".cate-list");
+      // val在哪个坐标范围内 
+      let cateList = document.querySelectorAll('.cate-list');
       for (let index = 0; index < this.pos.length; index++) {
         let pos1 = this.pos[index];
-        let pos2 = this.pos[index + 1];
-        if (val >= pos1 && val < pos2) {
+        let pos2 = this.pos[index+1];
+        if(val>= pos1 && val <pos2){
           //  console.log(index);
-          this.currentIndex = index;
+           this.currentIndex = index;
           //  参数element
-          this.cateScroll.scrollToElement(cateList[index], 300);
-          break;
+           this.cateScroll.scrollToElement(cateList[index],300)
+           break;
         }
       }
-    },
-  },
+    }
+  }
 };
 </script>
 
@@ -163,28 +164,29 @@ export default {
       p {
         flex: 1;
       }
-      .add-cart {
+      .add-cart{
         position: absolute;
-        right: 0.2rem;
-        bottom: 0;
-        .icon-jianhao {
-          color: #e2e3e5;
-          // 用于行元素 块级行元素的垂直对齐
+        right:0.2rem;
+        bottom:0;
+        .icon-jianhao{
+          color:#e2e3e5;
           vertical-align: middle;
+          font-size:  26px;
 
-          font-size: 26px;
         }
-        .icon-jiahao-copy {
-          color: #ffc134;
+        .icon-jiahao-copy{
+          color:#ffc134;
           vertical-align: middle;
           font-size: 26px;
+
         }
-        .num {
-          margin: 0 0.1rem;
+        .num{
+          margin:0 0.1rem;
           vertical-align: middle;
         }
       }
     }
   }
+
 }
 </style>
